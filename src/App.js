c4ca7918d0b6c753317cd1901ccd4ae071d7625b
Zokiio <joakim.hall1@gmail.com>
2020-09-21 01:36:11 +0900

    Add API url into code

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navigation from "./components/navigation";
 import Home from "./pages/home";
 import Blog from "./pages/blog";
-import Article from "./pages/article";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const App = () => {
   const [client, setClient] = useState(
     new ApolloClient({
-      uri: process.env.API_URI + "/graphql",
+      uri: "https://zottik-app.herokuapp.com/graphql",
       cache: new InMemoryCache(),
     })
   );
